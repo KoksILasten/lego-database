22619d20b3cb83dccec83d12152daa931ba4a479
Filip Malm-Bägén <filip.bagen@gmail.com>
2020-12-28 23:48:07 +0100

    Added pagination

--- search.php
+++ search.php
@@ -8,23 +8,34 @@
     // Check for provided search term from index
     if (isset($_GET['term'])) {
         $searchTerm = $_GET['term'];
-        $limit = 5;
+        $limit = 12;
 
         // Check wether the webiste should display the report or the sets
         if ($searchTerm == 'Rapport' OR $searchTerm == 'rapport') {
             header("Location: report.php");
-            die();
+            die(); // Ta bort??
 
         } else {
+            
             // Search for term in DB
-            $result	= mysqli_query(
-            $connection, "SELECT * FROM sets WHERE Setname LIKE '%{$searchTerm}%' OR SetID LIKE '%{$searchTerm}%' LIMIT $limit"
-        );
-        }
+            $fullSql = "SELECT * FROM sets WHERE Setname LIKE '%{$searchTerm}%' OR SetID LIKE '%{$searchTerm}%'";
+            $fullResult	= mysqli_query($connection, $fullSql);
+            $numberOfResults = mysqli_num_rows($fullResult);
+            $numberOfPages = ceil($numberOfResults / $limit);
 
-        
-       
-           
+            // find which page the user is on
+            if (!isset($_GET['page'])) {
+                $page = 1;
+            } else {
+                $page = $_GET['page'];
+            }
+
+            $thisPageFirstResult = ($page - 1) * $limit;
+
+            $sql = "SELECT * FROM sets WHERE Setname LIKE '%{$searchTerm}%' OR SetID LIKE '%{$searchTerm}%' LIMIT $thisPageFirstResult,$limit";
+            $result	= mysqli_query($connection, $sql);
+        }
+      
     }
     else {
         // Not found, send user back to index
@@ -35,6 +46,15 @@
 <div id='search_results_container'>
     <?php displayResults($connection, $result); ?>
 </div>
+
+<?php
+    // Pagination
+    echo "<div class='pagination'>";
+    for ($page = 1; $page <= $numberOfPages; $page++) {
+        echo '<a href="search.php?term=' . $searchTerm . '&page=' . $page . '">' . $page . '</a> ';
+    }
+    echo "</div>";
+?>
 
 <?php
     include "include/footer.php";
